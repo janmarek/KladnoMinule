5f11ee5d6f0c17fa18f0a9bd9877e844e71ea23e
Jan Marek <mail@janmarek.net>
2010-12-18 18:34:25 +0100

    tags

---
+++ app/Model/Tag/Service.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace KladnoMinule\Model\Tag;
+
+/**
+ * Tag service
+ *
+ * @author Jan Marek
+ */
+class Service extends \Neuron\Model\Service
+{
+	public function __construct($em)
+	{
+		parent::__construct($em, __NAMESPACE__ . "\Tag");
+	}
+
+
+
+	public function getFinder()
+	{
+		return new Finder($this);
+	}
+
+
+
+	public function getDictionary()
+	{
+		return $this->getFinder()->orderByName()->fetchPairs('id', 'name');
+	}
+
+
+
+	public function getUsedTags()
+	{
+		return $this->getFinder()->orderByName()->whereUsed()->getResult();
+	}
+
+}
