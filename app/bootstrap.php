03f073fdad490006ceb2c09fc4469239b9088d84
Jan Marek <mail@janmarek.net>
2010-12-18 18:38:56 +0100

    bootstrap - test mode

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -4,6 +4,10 @@ use Nette\Environment, Nette\Debug, Nette\Web\Html;
 
 require LIBS_DIR . '/Nette/loader.php';
 
+if (defined('TEST_MODE')) {
+	Environment::setName('test');
+}
+
 Debug::$strictMode = TRUE;
 Debug::enable();
 Html::$xhtml = false;
@@ -17,22 +21,22 @@ $serviceLoader->loadNeonConfigFiles(Environment::getContext(), array(
 	APP_DIR . "/services.neon",
 ));
 
-// error presenter
-$application = Environment::getApplication();
-$application->errorPresenter = 'Error';
-//$application->catchExceptions = TRUE;
-
-$router = $application->getRouter();
-
-$router[] = new Nette\Application\Route("<presenter>/<action>/<id>", array(
-	"presenter" => "Front:Homepage",
-	"action" => "default",
-	"id" => null,
-));
-
 Neuron\Form\FormMacros::register();
 
 // run
 if (!Environment::isConsole()) {
+	// error presenter
+	$application = Environment::getApplication();
+	$application->errorPresenter = 'Error';
+	//$application->catchExceptions = TRUE;
+
+	$router = $application->getRouter();
+
+	$router[] = new Nette\Application\Route("<presenter>/<action>/<id>", array(
+		"presenter" => "Front:Homepage",
+		"action" => "default",
+		"id" => null,
+	));
+
 	$application->run();
 }
